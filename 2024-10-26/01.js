e5d592b4c6bedca5f93c555efdee783da3d39bce
Alexax97 <alessioxale@gmail.com>
2024-10-27 11:57:26 +0100

    esercizio

---
+++ 2024-10-26/01.js
@@ -0,0 +1,42 @@
+
+let num1 = 10;
+
+let num2 = 20;
+
+console.log(num1);
+
+console.log(num2);
+
+console.log(`il valore della variabile num1 e ${num1}!!`);
+
+console.log(`il valore della variabile num2 e ${num2}!!`);
+
+
+console.log(num1+num2);
+
+
+let stringa = "sono una string";
+
+
+console.log(`il valore della variabile stringa e ${stringa}!!`);
+
+
+// let stringa = "sono di nuovo una stringa";
+
+// console.log(stringa);
+
+const PIGRECO = 3.14159265359
+
+console.log(`il valore di PIGRECO e ${PIGRECO}!!`);
+
+
+
+
+
+
+
+
+
+
+
+
